088fc175cef82b230efe6075d0b0de1612402117
TolaMironcenko <mironcenkotola@gmail.com>
2024-08-26 14:21:00 +0700

    added colors

--- sudo.sh
+++ sudo.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+RED="\033[1;31m"
+RESET="\033[0m"
+
 iterator=0
 while [ $iterator -lt 3 ]; do
     printf "password: "
     read -s pass
     echo
     echo $pass | su -c "$*" 2> /dev/null
-    if [ $? = 0 ]; then
+    res=$?
+    if [ $res = 0 ]; then
         exit 0
     fi
-    printf "ERROR: password is not correct\n"
+    if [ $res = 127 ]; then
+        printf "$RED** ERROR:$RESET command not found\n"
+        exit $?
+    fi
+    printf "$RED** ERROR:$RESET password is not correct\n"
     ((iterator++))
 done
 
